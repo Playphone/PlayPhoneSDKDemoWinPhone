27328edd5d4c62231f00cb9513b95370f8a9a79a
Anton usachov <antonu@playphone.com>
2012-02-14 14:43:24 +0200

    Room cookies integrated

--- WP7SDKDemo/views/RoomCookies.xaml.cs
+++ WP7SDKDemo/views/RoomCookies.xaml.cs
@@ -13,6 +13,8 @@ using Microsoft.Phone.Controls;
 
 namespace WP7SDKDemo.views
 {
+    using PlayPhone.MultiNet;
+
     public partial class RoomCookies : PhoneApplicationPage
     {
         public RoomCookies()
@@ -20,14 +22,52 @@ namespace WP7SDKDemo.views
             InitializeComponent();
         }
 
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            MNDirect.GetGameRoomCookiesProvider().GameRoomCookieDownloadFailed += RoomCookies_GameRoomCookieDownloadFailed;
+            MNDirect.GetGameRoomCookiesProvider().GameRoomCookieDownloadSucceeded += RoomCookies_GameRoomCookieDownloadSucceeded;
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatingFrom(System.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            MNDirect.GetGameRoomCookiesProvider().GameRoomCookieDownloadFailed -= RoomCookies_GameRoomCookieDownloadFailed;
+            MNDirect.GetGameRoomCookiesProvider().GameRoomCookieDownloadSucceeded -= RoomCookies_GameRoomCookieDownloadSucceeded;
+            base.OnNavigatingFrom(e);
+        }
+
+        void RoomCookies_GameRoomCookieDownloadSucceeded(int roomSFId, int key, string cookie)
+        {
+            MessageBox.Show("Cookie downloaded successfully! Key= " + key + " Value= " + cookie);
+        }
+
+        void RoomCookies_GameRoomCookieDownloadFailed(int roomSFId, int key, string error)
+        {
+            MessageBox.Show("Cookie download failed! " + error);
+        }
+
         private void upload(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                MNDirect.GetGameRoomCookiesProvider().SetCurrentGameRoomCookie(Int32.Parse(upload_key.Text), upload_data.Text);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("There should be integer value between 1 and 99!");
+            }
         }
 
         private void download(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                MNDirect.GetGameRoomCookiesProvider().GetCurrentGameRoomCookie(Int32.Parse(download_key.Text));
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("There should be integer value between 1 and 99!");
+            }
         }
     }
 }
